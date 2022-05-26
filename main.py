e12266134baf9931b8ab15c0203f58d4635424fd
meteor314 <meteor3141592@gmail.com>
2022-05-26 03:29:39 +0200

    Switch to new tab for apply

--- main.py
+++ main.py
@@ -24,36 +24,59 @@ class InitializeSelenium:
             "profile_path" : "/home/meteor314/.config/google-chrome-beta/Profile 1" 
         }
 
-        listURl = []
+        listURL = []
         i = searchOptions["start"]
-        while i <=100:
+        while i <=1000:
             link = "https://fr.indeed.com/jobs?q="+searchOptions['q']+"&l="+searchOptions['l']+"&start="+str(i)+"&jt="+searchOptions['jt']
             i+=10
-            listURl.append(link)
+            listURL.append(link)
 
         options = Options()
         options.binary_location = "/usr/bin/google-chrome-beta"
         options.add_argument("user-data-dir=" + searchOptions["profile_path"])   # Path to your chrome profile
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        driver.get(listURl[0])
-        driver.maximize_window()
-    
-
-        easyApply = driver.find_elements(By.CLASS_NAME, value="iaIcon")
+        driver.get(listURL[0])
+        driver.maximize_window() 
         
-        for e in easyApply:
-            # right click and open link in new tab
-            ActionChains(driver).context_click(e).key_down(Keys.CONTROL).click(e).perform()  
+        # switch to new tab for apply
+        j =1
+        while j< len(listURL):      
+
+            easyApply = driver.find_elements(By.CLASS_NAME, value="iaIcon")
+            print(len(easyApply))
+            print("J : ", j)           
+            for e in easyApply:
+                # right click and open link in new tab
+                ActionChains(driver).context_click(e).key_down(Keys.CONTROL).click(e).perform()  
+            
+            body = driver.find_element(By.TAG_NAME, value="body")
+            body.send_keys(Keys.CONTROL + 'n')
+            driver.get(listURL[j]) 
+            
             # switch to new tab
-            driver.switch_to.window(driver.window_handles[1])
+
+            i=0  
+            while i< len(easyApply):
+                driver.switch_to.window(driver.window_handles[-1])
+                time.sleep(1)                
+                driver.close()
+
+                driver.switch_to.window(driver.window_handles[-1])
+                i +=1  
+                
+            time.sleep(3)   
+
+
+            j+=1
+
+
+            # switch to new window
+            #driver.switch_to.window(driver.window_handles[-1])
+        
+            # switch to new tab for   search easy apply
+            
         
         time.sleep(600)
-
-
-
-
-        
-
 
 IndeedBot = InitializeSelenium() # create an instance of the class
 IndeedBot.initialize_selenium() # call the method
