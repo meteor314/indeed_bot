f4bbb56e63223addd7553a97be7cdaad7be4b5ec
meteor314 <meteor3141592@gmail.com>
2022-05-29 18:49:18 +0200

    Fixes bugs works now

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ from selenium.webdriver.support import expected_conditions as EC
 # import Action chains 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-import personalInfo
-
 
 # import applyForm
 import applyForm
@@ -89,11 +87,7 @@ class InitializeSelenium:
                     time.sleep(2)
 
                     # apply form
-                    self.applyForm()
-                   
-
-
-                
+                    self.applyForm() 
 
                 # close current tab
                 #self.driver.close()
@@ -123,7 +117,8 @@ class InitializeSelenium:
                 continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
                 currentURL = self.driver.current_url
                 while continueButton.is_displayed():
-                    if currentURL == "ttps://m5.apply.indeed.com/beta/indeedapply/form/questions/1" :
+                    if "questions" in currentURL :                     
+                        questionsURL = self.driver.current_url
                         time.sleep(2)
 
                         wait = WebDriverWait(self.driver, 10)
@@ -135,7 +130,12 @@ class InitializeSelenium:
                         continueButton.click()
                         continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
                         currentURL = self.driver.current_url
-                    elif currentURL == "https://m5.apply.indeed.com/beta/indeedapply/form/resume" :
+                        #verify the current url, if the currentURL is same as the questionsURL, then the form contains some obligatory fields, we can close the tab and continue to another job
+                        if questionsURL == currentURL:
+                            self.driver.close()
+                            self.driver.switch_to.window(self.driver.window_handles[-1])
+                            break
+                    elif "resume" in currentURL :
                         time.sleep(2)
 
                         print ("add letter or personnal documents")
@@ -148,23 +148,7 @@ class InitializeSelenium:
                         continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
                         currentURL = self.driver.current_url
 
-                    elif currentURL == "https://m5.apply.indeed.com/beta/indeedapply/form/work-experience":
-                        time.sleep(2)
-                        print ("add work experience")
-                        wait = WebDriverWait(self.driver, 10)
-                        jobTitle = wait.until(EC.element_to_be_clickable((By.ID, "jobTitle")))
-                        jobTitle.clear()
-                        time.sleep(2)
-                        jobTitle.send_keys(personalInfo.jobTitle).send_keys(Keys.ENTER)
-                        time.sleep(1)
-                        self.driver.find_element(By.ID, "companyName").send_keys(personalInfo.companyName).send_keys(Keys.ENTER)
-                        continueButton.click()
-
-                        time.sleep(2)
-                        continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
-                        currentURL = self.driver.current_url
-
-                    elif currentURL == "https://m5.apply.indeed.com/beta/indeedapply/form/review" :
+                    elif "review" in currentURL: # review
                         time.sleep(2)
                         print ("review and submit")
                         goToBottomOfPage = 'window.scrollTo(0, document.documentElement.scrollHeight)'  # scroll to the bottom of the page.
@@ -179,7 +163,7 @@ class InitializeSelenium:
 
 
                         time.sleep(2)
-                        continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
+                        continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton") 
                         currentURL = self.driver.current_url
 
                     else :
@@ -189,81 +173,7 @@ class InitializeSelenium:
                         continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
                         currentURL = self.driver.current_url
                         time.sleep(2)
-                        
-
-
-                    """match (currentURL) :
-                        case "https://m5.apply.indeed.com/beta/indeedapply/form/questions/1" : # personnal question or availability
-                            try : 
-                                print ("personnal question or availability")
-                                if self.driver.find_element(By.TAG_NAME, "textarea").is_displayed() :
-                                    self.driver.find_element(By.TAG_NAME, "textarea").send_keys(personalInfo.availability)                                      
-                                   
-                                    continueButton.click()
-                            except Exception as e:
-                                # continueButton does not work close this tabe and open a new one 
-                                print("Exception : ", e)               
-                                if (currentURL == "https://m5.apply.indeed.com/beta/indeedapply/form/questions/1") :
-                                    (self.driver.close())  
-                                        
-                                    
-                        case "https://m5.apply.indeed.com/beta/indeedapply/form/resume" : # add letter or personnal documents
-                            try :
-                                time.sleep(2)
-                                print ("add letter or personnal documents")
-
-                                wait = WebDriverWait(self.driver, 10)
-                                element = wait.until(EC.element_to_be_clickable((By.ID, "resume-display-buttonHeader")))
-                                continueButton.click()
-                            except Exception as e:
-                                print ("Error occured : " + str(e))
-                                (self.driver.close())  
-                                    
-                                    
-                        case "https://m5.apply.indeed.com/beta/indeedapply/form/work-experience" : # work experience
-                            try :
-
-                                time.sleep(2)
-                                print ("work experience")
-                                self.driver.find_element(By.ID, "jobTitle").send_keys(personalInfo.jobTitle)
-                                self.driver.find_element(By.ID, "companyName").send_keys(personalInfo.companyName)
-                                continueButton.click()
-                            except Exception as e:
-                                print ("Error occured : " + str(e))
-                                (self.driver.close())
-                                    
-                                    
-                            
-                        case "https://m5.apply.indeed.com/beta/indeedapply/form/review": # review and submit
-                            try :
-
-                                time.sleep(2)
-                                print ("review and submit")
-                                goToBottomOfPage = 'window.scrollTo(0, document.documentElement.scrollHeight)'  # scroll to the bottom of the page.
-                                self.driver.execute_script(goToBottomOfPage)
-                                continueButton.click()
-                            except Exception as e:
-                                print ("Error occured : " + str(e))
-                                (self.driver.close())
-                                   
-                                    
-                                    
-
-
-                        case _: #default case
-                            print("default case")
-                            currentURL = self.driver.current_url
-                                #continueButton.click()
-
-                            time.sleep(2)
-                            # if the current url is the same after click on continue button close the tab
-                            if (currentURL == self.driver.current_url) :
-                               (self.driver.close())
-
-
-                    continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
-                    currentURL = self.driver.current_url
-                       """             
+                              
                                 
                     
             #continueButton = self.driver.find_element(By.CLASS_NAME, "ia-continueButton")
