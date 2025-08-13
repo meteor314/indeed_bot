468d2ff2791647d25ea8b7aab7ec8d830476a21c
TechWithTy <tyrique1.daniel@gmail.com>
2025-08-13 12:33:15 -0600

    feat: add selector utilities for element finding and clicking with logging

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,3 +2,4 @@ from .indeed import domain_for_language, collect_indeed_apply_links, apply_to_jo
 from .logging_utils import setup_logger  # noqa: F401
 from .pagination import paginate_urls  # noqa: F401
 from .login import ensure_ppid_cookie  # noqa: F401
+from .selector import find_first, find_all, click_first  # noqa: F401
